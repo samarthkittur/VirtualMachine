56ab4ab1b3ebe593eca0af233522300ebdf05916
samarthkittur <86893940+samarthkittur@users.noreply.github.com>
2021-07-06 16:04:08 -0700

    Create examples.py

---
+++ VirtualMachine/examples.py
@@ -0,0 +1,13 @@
+def examples():
+    print("** Program 1: Runs the code for `print((2+3)*4)`")
+    Machine([2, 3, "+", 4, "*", "println"]).run()
+
+    print("\n** Program 2: Ask for numbers, computes sum and product.")
+    Machine([
+        '"Enter a number: "', "print", "read", "cast_int",
+        '"Enter another number: "', "print", "read", "cast_int",
+        "over", "over",
+        '"Their sum is: "', "print","+","println", 
+        '"Their product is: "', "print", "*", "println",
+        
+    ]).run()
