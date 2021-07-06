caa99dbc3326f2143ed3e65d2839651172da7a38
samarthkittur <86893940+samarthkittur@users.noreply.github.com>
2021-07-06 16:02:42 -0700

    Create vmrepl.py

---
+++ VirtualMachine/vmrepl.py
@@ -0,0 +1,13 @@
+def repl():
+    print('Hit CTRL+D or type "exit" to quit.')
+
+    while True:
+        try:
+            source = get_input("> ")
+            code = list(parse(source))
+            code = constant_fold(code)
+            Machine(code).run()
+        except (RuntimeError, IndexError) as e:
+            print("IndexError: %s" % e)
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt")
